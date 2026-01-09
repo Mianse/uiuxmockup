bfd529ff75c5f1dd2f11ea94c421355426ed2e7a
damian orina <96182161+Mianse@users.noreply.github.com>
2026-01-09 17:28:03 +0300

    feat: integrate Clerk for authentication and user management
    - Added ClerkProvider to layout for authentication context.
    - Implemented SignIn and SignUp pages using Clerk components.
    - Created API route for user management with database integration.
    - Established UserDetailContext for user state management.
    - Updated middleware to protect routes based on authentication status.
    - Added axios for API requests and user creation logic in Provider component.
    - Updated package.json and package-lock.json to include new dependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import {Button}  from '@/components/ui/button'
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
       <h2>Subscribe to damian</h2>
       <Button>Damian</Button>
+      <UserButton />
     </div>
   );
 }
