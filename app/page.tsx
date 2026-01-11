bbe6c06aa74a8d964ee4de393009de990f0522ac
damian orina <96182161+Mianse@users.noreply.github.com>
2026-01-11 05:10:10 +0300

    `feat: updated globals.css, page.tsx, components.json, schema.tsx, and middleware.ts`

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
-import {Button}  from '@/components/ui/button'
-import { UserButton } from "@clerk/nextjs";
+import Header from "./_shared/Header";
+import Hero from "./_shared/Hero";
 export default function Home() {
   return (
     <div>
-      <h2>Subscribe to damian</h2>
-      <Button>Damian</Button>
-      <UserButton />
+      <Header />
+      <Hero />
+      <div className='absolute -top-40 -left-40 h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full'/>
+      <div className='absolute -top-40 -right-[-200px] h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full'/>
+      <div className='absolute -bottom-[-200px] left-1/3 h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full'/>
+      <div className='absolute -top-[200px] left-1/2 h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full'/>
+
     </div>
   );
 }
---
+++ data/costant.tsx
@@ -0,0 +1,47 @@
+export const suggestions = [
+  {
+    icon: "🛒",
+    name: "UrbanCart Store",
+    description: "A modern ecommerce store for browsing, ordering, and tracking products online."
+  },
+  {
+    icon: "✅",
+    name: "Smart Todo Planner",
+    description: "An intelligent task manager that helps you plan, prioritize, and track daily goals."
+  },
+  {
+    icon: "📅",
+    name: "Daily Task Organizer",
+    description: "A simple planner to organize tasks, set reminders, and boost productivity."
+  },
+  {
+    icon: "🤖",
+    name: "AI Learning Hub",
+    description: "An AI-powered learning platform offering personalized courses and skill tracking."
+  },
+  {
+    icon: "💰",
+    name: "Finance Tracker Pro",
+    description: "A finance tracking app to monitor income, expenses, budgets, and savings."
+  },
+  {
+    icon: "📊",
+    name: "Expense Manager",
+    description: "An easy-to-use app for recording daily expenses and analyzing spending habits."
+  },
+  {
+    icon: "🍔",
+    name: "QuickBite Delivery",
+    description: "A fast and reliable food delivery app connecting users with nearby restaurants."
+  },
+  {
+    icon: "🧒",
+    name: "Kids Learning World",
+    description: "A fun and interactive learning app designed to educate kids through games."
+  },
+  {
+    icon: "✈️",
+    name: "Travel Planner Plus",
+    description: "A travel planning app for creating itineraries, managing bookings, and exploring destinations."
+  }
+];
